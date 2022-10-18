048f2a5c845f28f5af69714e365c97f05458f0b0
Soojung Hong <hong.soojung@gmail.com>
2022-10-18 22:49:03 +0200

    Update predict_all.py

--- predict_all.py
+++ predict_all.py
@@ -8,7 +8,8 @@ import numpy as np
 from PIL import Image
 from models import CompletionNetwork
 from utils import poisson_blend, gen_input_mask
-from models_with_dual_dilatedconv import DualCompletionNetwork  # if you used DualCompletionNetwork
+#from models_with_dual_dilatedconv import DualCompletionNetwork  # if you used DualCompletionNetwork
+from modified_models import CompletionNetwork_ModifiedDilation
 
 import glob
 import os
@@ -20,10 +21,10 @@ parser.add_argument('input_path')
 parser.add_argument('output_path')
 parser.add_argument('--max_holes', type=int, default=1)
 parser.add_argument('--img_size', type=int, default=256)  # 160)
-parser.add_argument('--hole_min_w', type=int, default=48)   #24
-parser.add_argument('--hole_max_w', type=int, default=64)   #48
-parser.add_argument('--hole_min_h', type=int, default=48)   #24
-parser.add_argument('--hole_max_h', type=int, default=64)   #48
+parser.add_argument('--hole_min_w', type=int, default=50)   #24
+parser.add_argument('--hole_max_w', type=int, default=50)   #48
+parser.add_argument('--hole_min_h', type=int, default=50)   #24
+parser.add_argument('--hole_max_h', type=int, default=50)   #48
 
 
 def predict(mpv, model, input_img, img_size, output_img):
@@ -33,7 +34,7 @@ def predict(mpv, model, input_img, img_size, output_img):
     img = transforms.RandomCrop((img_size, img_size))(img)
     x = transforms.ToTensor()(img)
     x = torch.unsqueeze(x, dim=0)
-    x = torch.cat((x, x, x), dim=1)  # convert to 3-channel format #SJ_FIX
+    #ORG x = torch.cat((x, x, x), dim=1)  # convert to 3-channel format #SJ_FIX
 
     # create mask
     mask = gen_input_mask(
@@ -52,7 +53,6 @@ def predict(mpv, model, input_img, img_size, output_img):
         input = torch.cat((x_mask, mask), dim=1)
         output = model(input)
         inpainted = poisson_blend(x_mask, output, mask)
-
         imgs = torch.cat((x, x_mask, inpainted), dim=0)
         save_image(imgs, output_img, nrow=3)
     print('output img was saved as %s.' % output_img)
@@ -61,8 +61,8 @@ def predict(mpv, model, input_img, img_size, output_img):
 def main(args):
     args.model = os.path.expanduser(args.model)
     args.config = os.path.expanduser(args.config)
-    args.input_img = os.path.expanduser(args.input_img)
-    args.output_img = os.path.expanduser(args.output_img)
+    args.input_img = os.path.expanduser(args.input_path)
+    args.output_img = os.path.expanduser(args.output_path)
 
     # =============================================
     # Load model
@@ -70,8 +70,9 @@ def main(args):
     with open(args.config, 'r') as f:
         config = json.load(f)
     mpv = torch.tensor(config['mpv']).view(1, 3, 1, 1)
-    # ORG model = CompletionNetwork()  # Specify the model class name
-    model = DualCompletionNetwork() # TODO : Specify the completion network
+    #model = CompletionNetwork()  # Specify the model class name
+    #model = DualCompletionNetwork() # TODO : Specify the completion network
+    model = CompletionNetwork_ModifiedDilation()
 
     # -------------------------------
     # if model is trained one gpu
@@ -88,7 +89,8 @@ def main(args):
     new_state_dict = OrderedDict()
     for k, v in state_dict.items():
         print('[debug] k : ', k)
-        name = k  # k[7:] # remove `module.`
+        #name = k  # k[7:] # remove `module.`
+        name = k[7:]
         new_state_dict[name] = v
         # load params
     model.load_state_dict(new_state_dict)
@@ -106,7 +108,15 @@ def main(args):
         output_img = predicted_image_path + fname
         predict(mpv, model, input_img, img_size, output_img)
 
+def resize(output_path):
+    for file in glob.glob(args.output_path + "*.png"):
+        only_filename = os.path.basename(file)
+        image = Image.open(file)
+        image = image.resize((768, 256), Image.ANTIALIAS)
+        image.save(fp=args.output_path + only_filename)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
+    resize(args.output_path)
